4c45ce43d8574355908cf271f808708221d88b83
Muneo42 <jayjay@yikes.nuts>
2022-06-21 09:32:55 -0500

    Vim settings updated

--- vim_settings/templates/template.sh
+++ vim_settings/templates/template.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 ## VARIABLES
-GREEN='\033[0;32m' # Green Color
-RED='\033[0;31m' # Red Color
-YELLOW='\033[33m' # Yellow Color
-NC='\033[0m' # No Color
-BOLD='\e[1m' # BOLD
-NONE='\e[0m' # Plain Text
+red='\033[0;31m' # Red Color
+green='\033[0;32m' # Green Color
+blue='\033[0;34m' # Blue Color
+yellow='\033[33m' # Yellow Color
+nc='\033[0m' # No Color
+bold='\e[1m' # BOLD
+none='\e[0m' # Plain Text
 
 ## FONCTIONS
 
